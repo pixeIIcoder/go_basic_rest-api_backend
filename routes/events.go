25d4543647214b835282e150192b5195b5c6bbcf
pixeIIcoder <yashwardhangupta502@gmail.com>
2026-02-17 23:30:27 +0530

    changes

--- routes/events.go
+++ routes/events.go
@@ -5,6 +5,7 @@ import (
 	"strconv"
 
 	"example.com/rest-api/models"
+	"example.com/rest-api/utils"
 	"github.com/gin-gonic/gin"
 )
 
@@ -32,8 +33,18 @@ func getEvent(context *gin.Context) {
 }
 
 func createEvent(context *gin.Context) {
+	token := context.Request.Header.Get("Authorization")
+	if token == "" {
+		context.JSON(http.StatusUnauthorized, gin.H{"message": "No token not authorized"})
+		return
+	}
+	err := utils.VerifyToken(token)
+	if err != nil {
+		context.JSON(http.StatusUnauthorized, gin.H{"message": "No token not authorized", "error": err.Error()})
+		return
+	}
 	var event models.Event
-	err := context.ShouldBindJSON(&event)
+	err = context.ShouldBindJSON(&event)
 
 	if err != nil {
 		context.JSON(http.StatusBadRequest, gin.H{"message": "Could not Parse the request"})
--- utils/jwt.go
+++ utils/jwt.go
@@ -1,6 +1,7 @@
 package utils
 
 import (
+	"errors"
 	"time"
 
 	"github.com/golang-jwt/jwt/v5"
@@ -16,4 +17,29 @@ func GenerateToken(email string, userId int64) (string, error) {
 	})
 
 	return token.SignedString([]byte(secretKey))
+}
+func VerifyToken(tokenString string) error {
+	parsedtoken, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, jwt.ErrSignatureInvalid
+		}
+
+		return []byte(secretKey), nil
+	})
+	tokenIsValid := parsedtoken.Valid
+	if err != nil {
+		return errors.New("Could not parse the Token")
+	}
+	if !tokenIsValid {
+		return errors.New("Invalid Token")
+	}
+	_, ok := parsedtoken.Claims.(jwt.MapClaims)
+	if !ok {
+		return errors.New("Invalid token Claims")
+	}
+
+	// email := claims["email"].(string)
+	// userId := claims["userId"].(int64)
+	return nil
 }
